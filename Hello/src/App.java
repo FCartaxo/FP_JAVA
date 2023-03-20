7172b0825c0ee37f8ed051458922a2c404fcd99a
pv23789 <pv23789@estgl.ipv.pt>
2023-03-20 10:24:44 +0000

    Investimento_commit

--- Hello/src/App.java
+++ Hello/src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        double principal;
+        double rate;
+        double interest;
+
+        Scanner stdin = new Scanner (System.in);
+
+        System.out.println("Investimento após um ano");
+        System.out.print("                  Investimento inicial: ");
+        principal = stdin.nextDouble();
+        System.out.print("    Taxa de juros anual (como decimal): ");
+        rate = stdin.nextDouble();
+
+        interest = principal * rate;
+        principal += interest;
+
+        System.out.printf("       O valor do juro ganho é %10.2f Eur%n", interest);
+        System.out.format("O valor do investimento após é %10.2f Eur%n", principal);
+
     }
 }
